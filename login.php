d645cc8ccb7e8467250a6119a1a46ed2e3ac4a4d
1126200193@qq.com <1126200193@qq.com>
2019-11-20 20:07:25 +0800

    second commit

--- login.php
+++ login.php
@@ -123,7 +123,66 @@
 
     </div>
 
+
+
+
+    <div class="center-mess" id="center-mess">
+        <!--网页留言部分以及用户信息部分-->
+        <div class="center-mess_1">
+            <div class="row">
+                <!--                  留言模块-->
+                <div class="col-md-8">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>留言模块</span>
+                            <i id="rotate" class="el-icon-refresh-right"  style="float:right; padding: 3px 0;font-size:16px;cursor: pointer;"></i>
+                        </div>
+                        <div >
+                            <el-form action="sava.php"  method="POST" :label-position="labelPosition" label-width="80px">
+                                <el-form-item label="用户名">
+                                    <el-input id="username_go"  v-model="customname"></el-input>
+                                </el-form-item>
+                                <el-form-item label="账号">
+                                    <el-input id="mail" v-model="number"></el-input>
+                                </el-form-item>
+                                <el-form-item label="留言内容">
+                                    <el-input type="textarea" id="message" v-model="message"></el-input>
+                                </el-form-item>
+                                <el-form-item>
+                                    <el-button type="primary" @click="submitForm()">提交</el-button>
+                                    <el-button @click="resetForm()">重置</el-button>
+                                </el-form-item>
+                            </el-form>
+                        </div>
+                    </el-card>
+                </div>
+
+                <!--个人信息模块-->
+                <div class="col-md-4">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>个人信息</span>
+                            <i id="rotate" class="el-icon-refresh-right" style="float:right; padding: 3px 0;font-size:16px;cursor: pointer;"></i>
+                        </div>
+                        <div>
+                            <div class="text">
+                                <span>用户名：</span>
+                                <span>{{customname}}</span>
+                            </div>
+                            <div class="text item">
+                                <span>用户账户：</span>
+                                <span>{{number}}</span>
+                            </div>
+                        </div>
+                    </el-card>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+</div>
+
 
 </div>
 
@@ -172,6 +231,8 @@
                                 center:true,
                                 type:'success',
                             });
+                            this.goinusermess();
+                            this.returnjson();
                         }
                         else {
                             this.$message({
@@ -221,6 +282,128 @@
                 });
                 return false;
             },
+
+            //载入用户信息方法
+            //时间：2019.11.14
+            goinusermess:function(){
+                $.post({
+                    url:'1.php',
+                    dataType:'json',
+                    data:{'action':'user'},
+                    success:(data)=>{
+                        this.userid = data['userid'];
+                        this.customname = data['customname'];
+                        this.number = data['username']
+                    },
+                    error:()=>{
+                        this.$message({
+                            message:'请先登录！',
+                            center: true,
+                            type: 'warning'
+                        });
+                    }
+                })
+            },
+
+            //请求returnjson.php页面返回留言数据（ajax）
+            //时间：2019.11.15
+            returnjson:function() {
+                $.post({
+                    url:'returnjson.php',
+                    dataType: "json",
+                    success:(data)=>{
+                        this.isLoading = true;
+                        setTimeout(()=>{
+                            this.list = data;
+                            this.total = data.length;
+                            this.isLoading = false;
+                        },3000);
+                    },
+                    error: ()=>{
+                        alert("注册失败");
+                    }
+                });
+                return false;
+            },
+
+            //时间戳处理
+            //时间：2019.11.15
+            dateFormat:function(row, column) {
+                // var date = row[column.property];
+                // if (date == undefined) {
+                //     return "";
+                // }
+                // console.log(moment(date).format("YYYY-MM-DD HH:mm:ss"));
+                // return moment(date).format("YYYY-MM-DD HH:mm:ss");
+                let date = new Date(parseInt(row.utime) * 1000);
+                let Y = date.getFullYear() + '-';
+                let M = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) + '-' : date.getMonth() + 1 + '-';
+                let D = date.getDate() < 10 ? '0' + date.getDate() + ' ' : date.getDate() + ' ';
+                let h = date.getHours() < 10 ? '0' + date.getHours() + ':' : date.getHours() + ':';
+                let m = date.getMinutes()  < 10 ? '0' + date.getMinutes() + ':' : date.getMinutes() + ':';
+                let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+                return Y + M + D + h + m + s;
+            },
+
+
+            //提交留言模块表单数据到sava.php页面(需要增加逻辑处理当用户登录后才可以进行留言)
+            //时间：2019.11.17
+            submitForm:function(){
+                $.post({
+                    url:'save.php',
+                    dataType: "json",
+                    data: {
+                        'username':$('#username_go').val(),
+                        'mail': $('#mail').val(),
+                        'message': $('#message').val(),
+                    },
+                    success:(data)=>{
+                        if(data['code']){
+                            this.$message({
+                                message:data['code'],
+                                center: true,
+                                type: 'success'
+                            });
+                            this.returnjson();
+                        }else{
+                            this.$message({
+                                message:data['code'],
+                                center: true,
+                                type: 'warning'
+                            });
+                        }
+                    },
+                    error: ()=>{
+                        alert("失败");
+                    }
+                });
+                return false;
+            },
+
+
+            //提交留言重置方法
+            //时间：2019.11.17
+            resetForm:function() {
+                this.$message({
+                    message:'正在开发中......',
+                    center: true,
+                    type: 'warning'
+                })
+            },
+
+            //分页处理方法
+            changePage:function(currentPage) {
+                this.currentPage = currentPage;
+            },
+
+            //留言加载v-loding方法
+            isloading:function(){
+                this.isLoading = true;
+                setTimeout(() => {
+                    this.isLoading = false;
+                },3000);
+            }
+
 
         },
     })
